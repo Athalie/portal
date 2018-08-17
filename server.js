cca0ed580bdbbf9ae08562c04baf93f0dee641bb
athalie <athalie@yandex.ru>
2018-08-17 13:28:42 +0300

    переадресация через https

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const path = require('path');
 
 const app = express();
 
+const env = process.env.NODE_ENV || 'development';
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+
+if (env === 'production') {
+  app.use(forceSSL());
+}
+
 app.use(express.static('./dist/portal'));
 
 app.get('/*', function(req,res) {
